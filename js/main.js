cac2b8c26ab85f3acb710d432b3903b3cfa0991a
Klima András <klima.andras@gmail.com>
2020-12-16 13:22:56 +0100

    object created

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 function page_load() {
   let root = document.querySelector("#root");
 
-  console.log(root);
+  // console.log(root);
+
+  let myObjects = [
+    {
+      tag: "h1",
+      content: "Moby",
+    },
+    {
+      tag: "p",
+      content: "Frontend Developer"
+    }
+  ];
+
+  // console.log(myObjects);
 }
 
 window.addEventListener("load", page_load);
