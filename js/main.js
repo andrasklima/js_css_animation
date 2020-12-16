dcd2dec325eb61d9686949ca053a3519934732a8
Klima András <klima.andras@gmail.com>
2020-12-16 13:58:47 +0100

    tag added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+function getTagContentHTML(tag, content) {
+  let result = "";
+
+  let startTag = `<${tag}>`;
+  let endTag = `</${tag}>`;
+
+  result += startTag;
+  result += content;
+  result += endTag;
+
+  return result;
+}
+
 function page_load() {
   let root = document.querySelector("#root");
 
@@ -10,11 +23,41 @@ function page_load() {
     },
     {
       tag: "p",
-      content: "Frontend Developer"
-    }
+      content: "Frontend Developer",
+    },
   ];
 
   // console.log(myObjects);
+
+  /* 
+  for (let index = 0; index < myObjects.length; index++) {
+    const element = myObjects[index];
+  }
+*/
+
+  for (myObject of myObjects) {
+    root.insertAdjacentHTML(
+      "beforeend",
+      ` <${myObject.tag}>
+          ${myObject.content}
+        </${myObject.tag}>`
+    );
+
+    /* 
+    root.insertAdjacentHTML(
+      "beforeend",
+      getTagContentHTML(myObject.tag, myObject.content)
+    );
+    */
+
+    // console.log(myObject.tag);
+  }
+
+  /* 
+  myObjects.forEach(function (element) {
+    console.log(element);
+  });
+*/
 }
 
 window.addEventListener("load", page_load);
